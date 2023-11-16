bf548a62e7dd3641408c96efa0a9f2b4a75162d0
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2023-11-16 18:15:15 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 6 ms, Memory: 13,540 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int T;
+	
+	cin>>T;
+	
+	for(int i = T; i >= 0; --i)
+	{
+
+		cout << i << " ";
+
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
