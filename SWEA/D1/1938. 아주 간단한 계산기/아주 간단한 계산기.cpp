e1df2b7248fbe870a07f40141504eff7c0fa6e96
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2023-11-16 18:14:07 +0900

    [D1] Title: 아주 간단한 계산기, Time: 6 ms, Memory: 13,536 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int A;
+    int B;
+    
+    cin >> A >> B;
+	
+	cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
