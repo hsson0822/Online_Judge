473e16913ad8e779d93ab0ad7698fecead0bacba
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2023-11-20 18:39:20 +0900

    [D3] Title: 조별과제, Time: 88 ms, Memory: 13,544 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/조별과제.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin>>T;
+
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		int people;
+        cin >> people;
+        
+        
+		cout << "#" << test_case << " " << people / 3 << "\n";
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
