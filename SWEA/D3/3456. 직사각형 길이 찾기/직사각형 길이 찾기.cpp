026c8b53855ef0da150cf58f4278cddf98ce5a1a
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2023-11-27 21:39:21 +0900

    [D3] Title: 직사각형 길이 찾기, Time: 93 ms, Memory: 13,556 KB -BaekjoonHub

--- SWEA/D3/3456. 직사각형 길이 찾기/직사각형 길이 찾기.cpp
+++ SWEA/D3/3456. 직사각형 길이 찾기/직사각형 길이 찾기.cpp
@@ -1,36 +1,34 @@
 #include<iostream>
-#include<map>
-
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	
+
 	cin>>T;
-	
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-        map<int,int> nums;
-        int leng;
-        
-		for(int i = 0 ; i < 3; ++i)
-        {
-            int n;
-            cin >> n;
-            nums[n]++;
-        }
-        
-        for(auto m : nums)
-        {
-            if(m.second % 2 == 1)
-            {
-                leng = m.first;
-                break;
-            }
-        }
-        cout << "#" << test_case << " " << leng << "\n";
-	}
+
+	for (test_case = 1; test_case <= T; ++test_case)
+    {
+		vector<int> nums;
+
+		for (int i = 0; i < 3; ++i)
+		{
+			int n;
+			cin >> n;
+
+			auto iter = find(nums.begin(), nums.end(), n);
+			if(iter != nums.end())
+			{
+				nums.erase(iter, iter + 1);
+			}
+			else
+				nums.push_back(n);
+		}
+
+        cout << "#" << test_case << " " << nums[0] << "\n";
+    }
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
