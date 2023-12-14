d869313d7962c29fa39ebb428e54ce96aa5592da
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2023-12-14 18:19:43 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    cout << "134" <<endl;
+    cout << "hsson0822" << endl;
+    return 0;
+}
