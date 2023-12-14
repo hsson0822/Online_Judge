cb6ff73bbb5f969c0e989a2d2434ef2b5670605b
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2023-12-14 18:14:59 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    string str;
+	while (getline(cin,str))
+		cout << str << endl;
+    
+    return 0;
+}
