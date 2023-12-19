3291233b1a3ac85be3a1733abff45279422730cd
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2023-12-19 21:23:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    int n;
+	cin >> n;
+
+	vector<int> dp;
+	dp.push_back(0);
+	dp.push_back(1);
+	dp.push_back(2);
+
+	for (int i = 3; i <= n; ++i)
+	{
+		dp.push_back((dp[i-1] + dp[i-2]) % 10007);
+	}
+
+	cout << dp[n] << endl;
+    
+    return 0;
+}
