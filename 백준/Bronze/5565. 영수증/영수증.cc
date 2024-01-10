58dad318edfe4d58a0f2c7ec802894c0d4f9f382
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2024-01-10 18:19:05 +0900

    [Bronze III] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int sum;
+	cin >> sum;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		int n;
+		cin >> n;
+		sum -= n;
+	}
+
+	cout << sum;
+
+	return 0;
+}
