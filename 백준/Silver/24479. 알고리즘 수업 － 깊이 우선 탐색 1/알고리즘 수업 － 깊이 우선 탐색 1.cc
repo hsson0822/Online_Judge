1c33ac3c829cf898e1cf293751159009053390cb
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2024-01-31 16:14:24 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 88 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001];
+int cnt;
+
+void dfs(int start)
+{
+	visited[start] = cnt++;
+
+	for (int i = 0; i < graph[start].size(); ++i)
+	{
+		int x = graph[start][i];
+
+		if (visited[x] == 0)
+		{
+			dfs(x);
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int node;
+	int line;
+	int start;
+	cin >> node >> line >> start;
+
+	for (int i = 0; i < line; ++i)
+    {
+	    int a;
+	    int b;
+	    cin >> a >> b;
+
+	    graph[a].push_back(b);
+	    graph[b].push_back(a);
+
+    }
+
+    for (int i = 1; i <= node; ++i)
+    {
+	    sort(graph[i].begin(), graph[i].end());
+    }
+
+	cnt = 1;
+	dfs(start);
+
+	for (int i = 1; i <= node; ++i)
+	{
+		cout << visited[i] << "\n";
+	}
+
+	return 0;
+}
