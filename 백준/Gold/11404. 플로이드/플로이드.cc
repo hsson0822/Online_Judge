6149a26ad2fa8d132933a6dd003de942c3cef3e7
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2024-06-27 13:43:05 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,83 +1,63 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 
 using namespace std;
 
-const long long INF = 987654321;
-
-int N;
-int M;
-
-long long costs[101][101];
-vector<pair<int,long long>> route[101];
-
-void dijkstra(int start)
-{
-	priority_queue<pair<int, long long>> PQ;
-	PQ.push(pair(start, 0));
-	costs[start][start] = 0;
-
-	while (!PQ.empty())
-	{
-		int cur = PQ.top().first;
-		long long cost = PQ.top().second;
-		PQ.pop();
-
-		for (int i = 0; i < route[cur].size(); ++i)
-		{
-			int next = route[cur][i].first;
-			long long nCost = route[cur][i].second;
-
-			if (costs[start][next] > cost + nCost)
-			{
-				costs[start][next] = cost + nCost;
-				PQ.push(pair(next, costs[start][next]));
-			}
-		}
-	}
-	
-}
+const int INF = 987654321;
+int table[101][101];
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	cin >> N;
-	cin >> M;
+    int N;
+    cin >> N;
 
-	for (int i = 0; i < M; ++i)
-	{
-		int a;
-		int b;
-		long long w;
-		cin >> a >> b >> w;
-		route[a].push_back(pair(b, w));
-	}
-
-	for (int i = 1; i <= N; ++i)
-	{
-		for (int j = 1; j <= N; ++j)
-		{
-			costs[i][j] = INF;
-		}
-	}
-
-	for (int i = 1; i <= N; ++i)
-		dijkstra(i);
-
-	for (int i = 1; i <= N; ++i)
+    int M;
+    cin >> M;
+    for (int i = 1; i <= N; ++i)
     {
-	    for (int j = 1; j <= N; ++j)
-	    {
-		    if (costs[i][j] == INF)
-			    cout << "0 ";
-		    else
-			    cout << costs[i][j] << " ";
-    	}
-	    cout << endl;
+        for (int j = 1; j <= N; ++j)
+        {
+            if (i == j)
+                table[i][j] = 0;
+            else
+                table[i][j] = INF;
+        }
+    }
+
+    for(int i = 0 ; i < M ; ++i)
+    {
+        int a;
+        int b;
+        int w;
+        cin >> a >> b >> w;
+        table[a][b] = min(table[a][b], w);
+    }
+
+    for (int k = 1; k <= N; ++k)
+    {
+        for (int i = 1; i <= N; ++i)
+        {
+            for (int j = 1; j <= N; ++j)
+            {
+                if (table[i][j] > table[i][k] + table[k][j])
+                    table[i][j] = table[i][k] + table[k][j];
+            }
+        }
+    }
+
+    for (int i = 1; i <= N; ++i)
+    {
+        for (int j = 1; j <= N; ++j)
+        {
+            if (table[i][j] == INF)
+                cout << 0 << " ";
+            else
+                cout << table[i][j] << " ";
+        }
+        cout << endl;
     }
 
 	return 0;
