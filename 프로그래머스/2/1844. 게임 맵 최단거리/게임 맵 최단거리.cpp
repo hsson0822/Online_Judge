78be1f50bb44e50459ba6320f811db469d47df0f
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2024-10-04 19:40:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.27 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,56 +1,46 @@
-#include <vector>
-#include <queue>
+#include<vector>
+#include<queue>
 
 using namespace std;
 
-int N;
-int M;
-int dist[101][101];
-int dirX[4] {1,0,-1,0};
-int dirY[4] {0,1,0,-1};
-const int INF = 987654321;
+int visited[101][101];
+int dx[4]{1,0,-1,0};
+int dy[4]{0,-1,0,1};
 
-void bfs(vector<vector<int> > maps)
+int solution(vector<vector<int> > maps)
 {
-    queue<pair<int,int>> q;
-    dist[0][0] = 1;
-    q.push(pair(0,0));
+    int answer = 0;
+    int n = maps.size();
+    int m = maps[0].size();
     
-    while(!q.empty())
+    queue<pair<int,int>> Q;
+    visited[0][0] = 1;
+    Q.push(pair(0,0));
+    
+    while(!Q.empty())
     {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
+        int x = Q.front().first;
+        int y = Q.front().second;
+        Q.pop();
         
         for(int i = 0 ; i < 4 ; ++i)
         {
-            int nx = x + dirX[i];
-            int ny = y + dirY[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             
-            if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-            if(maps[nx][ny] == 0) continue;
-            if(dist[nx][ny] != INF) continue;
-            if(dist[x][y] +1 > dist[nx][ny]) continue;
+            if(nx < 0 || ny <0 || nx>=n || ny >= m) continue;
+            if(maps[nx][ny] != 1) continue;
+            if(visited[nx][ny] != 0) continue;
             
-            dist[nx][ny] = dist[x][y]+1;
-            q.push(pair(nx,ny));
+            visited[nx][ny] = visited[x][y]+1;
+            Q.push(pair(nx,ny));
         }
-        
     }
-}
-
-int solution(vector<vector<int> > maps)
-{   
-    N = maps.size();
-    M = maps[0].size();
-    for(int i = 0 ; i < N; ++i)
-        for(int j = 0 ; j < M ; ++j)
-            dist[i][j] = INF;
     
-    bfs(maps);
-        
-    if(dist[N-1][M-1] == INF)
-        return -1;
+    if(visited[n-1][m-1] !=0)
+        answer = visited[n-1][m-1];
     else
-        return dist[N-1][M-1];
+        answer = -1;
+    
+    return answer;
 }
