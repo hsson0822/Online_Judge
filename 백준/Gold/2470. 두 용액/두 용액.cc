7914cfefc89c652aef95b02fa1e3035b86e3bd27
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2025-02-12 16:00:08 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cc
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -1,38 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include <algorithm>
-#define INF 2000000000
+
 using namespace std;
 
-vector<int> answer(2);
-int main() {
-    int N;
-    cin >> N;
-    vector<int> arr(N);
-    for (int i = 0; i < N; ++i)
-        cin >> arr[i];
-    sort(arr.begin(), arr.end()); // 정렬 필수!
+int n;
+vector<int> liquid;
+int miniest = 2000000000;
 
-    int start = 0; // start 은 왼쪽 끝에서
-    int end = N - 1; // end 은 오른쪽 끝에서 시작
-    int min = INF; // 현재까지 0 에 가장 가까웠던 합
-    while (start < end) {
-        int sum = arr[start] + arr[end];
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-        if (min > abs(sum)) {
-            min = abs(sum);
-            answer[0] = arr[start];
-            answer[1] = arr[end]; 
+	cin >> n;
 
-            if (sum == 0)
-                break;
-        }
+	for (int i = 0; i < n; ++i)
+	{
+		int n;
+		cin >> n;
+		liquid.push_back(n);
+	}
 
-        if (sum < 0) 
-            start++;
-        else 
-            end--;
-    }
+	sort(liquid.begin(), liquid.end());
 
-    cout << answer[0] << " " << answer[1];
+	int left = 0;
+	int right = n - 1;
+
+	int leftAns{};
+	int rightAns{};
+	
+
+	while (left < right)
+	{
+		int m = liquid[left] + liquid[right];
+
+		if (abs(m) < miniest)
+		{
+			leftAns = liquid[left];
+			rightAns = liquid[right];
+			miniest = abs(m);
+
+			if (m == 0)
+				break;
+		}
+
+		if (m > 0)
+		{
+			--right;
+		}
+		else
+		{
+			++left;
+		}
+
+
+	}
+
+	cout << leftAns << " " << rightAns;
+
+	return 0;
 }
+
