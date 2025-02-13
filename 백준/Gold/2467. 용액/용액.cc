95152b5e0032ac9c2cfce8870df6755f4759f4d1
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2025-02-13 14:28:30 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int n;
+vector<int> liquid;
+int miniest = 2000000000;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		int n;
+		cin >> n;
+		liquid.push_back(n);
+	}
+
+	int left = 0;
+	int right = n - 1;
+
+	int leftAns{};
+	int rightAns{};
+	
+
+	while (left < right)
+	{
+		int m = liquid[left] + liquid[right];
+
+		if (abs(m) < miniest)
+		{
+			leftAns = liquid[left];
+			rightAns = liquid[right];
+			miniest = abs(m);
+
+			if (m == 0)
+				break;
+		}
+
+		if (m > 0)
+		{
+			--right;
+		}
+		else
+		{
+			++left;
+		}
+
+
+	}
+
+	cout << leftAns << " " << rightAns;
+
+	return 0;
+}
+
