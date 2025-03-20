f3826b1141cae99b728d0b655a0738ce8889ec52
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2025-03-20 23:14:30 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int w, int num) {
+    int answer = 0;
+    while (num <= n)
+    {
+        num += 2 * (w - ((num - 1) % w) - 1) + 1;
+        answer ++;
+    }
+    return answer;
+}
