8b00454d2ce1018b480ee9693f0dac3ee71a4b20
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2025-04-24 15:49:56 +0900

    [Gold V] Title: 풍선 맞추기, Time: 56 ms, Memory: 9392 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <climits>
+#include <math.h>
+#include <cstring>
+
+using namespace std;
+
+int arrow[1000001];
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	vector<int> nums(N);
+
+	for (int i = 0; i < N; ++i)
+		cin >> nums[i];
+
+	int answer{};
+	for (int i = 0; i < N; ++i)
+	{
+		if (arrow[nums[i]] == 0)
+		{
+			++arrow[nums[i] - 1];
+			++answer;
+			continue;
+		}
+
+		--arrow[nums[i]];
+		++arrow[nums[i] - 1];
+	}
+
+	cout << answer;
+
+	return 0;
+}
